665c2ca7b2e564f8fdbcf8ffc57865ab76dfffb9
Agam Agarwal <agammaster@gmail.com>
2016-04-09 14:35:41 +0530

    Changed to version 0.1.3. Added autocomplete support

--- put-cli.rb
+++ put-cli.rb
@@ -1,13 +1,15 @@
 class PutCli < Formula
   desc "Command-line utility for put.io"
   homepage "https://github.com/AjayBrahmakshatriya/put-cli/"
-  url "https://github.com/AjayBrahmakshatriya/put-cli/archive/0.1.2.tar.gz"
-  sha256 "54598253ef6a082884263818ea053a96b0406d9d994668590203af87134f8a37"
+  url "https://github.com/AjayBrahmakshatriya/put-cli/archive/0.1.3.tar.gz"
+  sha256 "5a46a4e55694491e49d60390c33046021984d8293556a6a7804fc8fcfe34f129"
 
   def install
     system "make"
     system "make", "install"
     bin.install "build/put"
+    bash_completion.install "build/autocomplete_script" => "put"
+    (bash_completion/"helpers").install "build/autocomplete" => "put"
   end
 
   test do

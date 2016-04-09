dc876b5b0cf2cfe6abf1e6966602577b8e848c54
Agam Agarwal <agammaster@gmail.com>
2016-04-09 14:43:26 +0530

    Changed to version 0.1.4

--- put-cli.rb
+++ put-cli.rb
@@ -1,8 +1,8 @@
 class PutCli < Formula
   desc "Command-line utility for put.io"
   homepage "https://github.com/AjayBrahmakshatriya/put-cli/"
-  url "https://github.com/AjayBrahmakshatriya/put-cli/archive/0.1.3.tar.gz"
-  sha256 "5a46a4e55694491e49d60390c33046021984d8293556a6a7804fc8fcfe34f129"
+  url "https://github.com/AjayBrahmakshatriya/put-cli/archive/0.1.4.tar.gz"
+  sha256 "cfc157725043a21452b6867ecbe936049076c04751bdbdc8f9a78632f57c933c"
 
   def install
     system "make"

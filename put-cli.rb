b32cdbc407e32f670c1b5a5fd6999a09a75e4d8e
Agam Agarwal <agammaster@gmail.com>
2016-04-19 01:10:59 +0530

    Changed to version 0.1.5 (rm command is now supported)

--- put-cli.rb
+++ put-cli.rb
@@ -1,8 +1,8 @@
 class PutCli < Formula
   desc "Command-line utility for put.io"
   homepage "https://github.com/AjayBrahmakshatriya/put-cli/"
-  url "https://github.com/AjayBrahmakshatriya/put-cli/archive/0.1.4.tar.gz"
-  sha256 "cfc157725043a21452b6867ecbe936049076c04751bdbdc8f9a78632f57c933c"
+  url "https://github.com/AjayBrahmakshatriya/put-cli/archive/0.1.5.tar.gz"
+  sha256 "ab31c8b4dead60ccba2b6652c5ce1f67d81747cfde36398d6f80abe1e05727fb"
 
   def install
     system "make"

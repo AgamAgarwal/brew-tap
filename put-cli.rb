168a1b1acd3f9bf96fae436d3ac535afe0411203
Agam Agarwal <agammaster@gmail.com>
2018-05-25 20:22:05 +0530

    Explicitly mention python@2 as python is now deprecated

--- put-cli.rb
+++ put-cli.rb
@@ -4,7 +4,7 @@ class PutCli < Formula
   url "https://github.com/LordWaldomort/put-cli/archive/0.3.1.tar.gz"
   sha256 "899a1d1046f4b6c83b4edf7220566829de3c048af2c454563cfd2181d02b116d"
 
-  depends_on "arghandler" => :python
+  depends_on "arghandler" => :python@2
 
   def install
     system "make"
